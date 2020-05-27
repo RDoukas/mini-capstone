523fe50d4ec68693d9bfc635f0f1b759c3755fd6
Becca <rrdoukas@gmail.com>
2020-05-27 12:01:20 -0500

    adds new product instance

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,8 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 
+
+product = Product.new(name: "Coffee", price: 10, image_url: "https://www.google.com/search?q=coffee&rlz=1C5CHFA_enUS899US899&sxsrf=ALeKk033UpIZP2k_g0nZ_naUu_wjQiZfiA:1590598343216&source=lnms&tbm=isch&sa=X&ved=2ahUKEwit0-jJwNTpAhUQXM0KHZgADXkQ_AUoAnoECBkQBA&biw=640&bih=614&dpr=2#imgrc=Kqh2xJ1fV5GXoM", description: "Fresh roasted Arabica beans from Chile. Has a chocolate, nutty finish.")
+
+product.save
