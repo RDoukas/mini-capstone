b4d583357a354e968dcd918df5c8a8676f314322
Becca <rrdoukas@gmail.com>
2020-05-27 14:03:39 -0500

    updates controllers

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,12 +1,12 @@
 class Api::ProductsController < ApplicationController
 
   def all_products_action
-    @products = Product.all
+    @product = Product.all
     render "all_products.json.jb"
   end 
 
   def single_product_action
-    @single_product = Product.all.sample
+    @product = Product.all.sample
     render "single_product.json.jb"
 
   end 
