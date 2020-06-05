b6b18466570fba09685ea8fc47cbc29e741a98ac
Becca <rrdoukas@gmail.com>
2020-06-04 19:04:09 -0500

    adds more data

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -19,6 +19,7 @@ class Api::ProductsController < ApplicationController
       description: params[:description]
     )
 
+  
     if @product.save
       render "show.json.jb"
     else 
@@ -34,6 +35,8 @@ class Api::ProductsController < ApplicationController
     @product.price = params[:price] || @product.price
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
+    
+
     
     if @product.save
       render "show.json.jb"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-# 
-
-# product = Product.new(name: "Coffee", price: 10, image_url: "https://www.google.com/search?q=coffee&rlz=1C5CHFA_enUS899US899&sxsrf=ALeKk033UpIZP2k_g0nZ_naUu_wjQiZfiA:1590598343216&source=lnms&tbm=isch&sa=X&ved=2ahUKEwit0-jJwNTpAhUQXM0KHZgADXkQ_AUoAnoECBkQBA&biw=640&bih=614&dpr=2#imgrc=Kqh2xJ1fV5GXoM", description: "Fresh roasted Arabica beans from Chile. Has a chocolate, nutty finish.")
-
-# product.save
-
-
-
-# product = Product.new(name: "Rake", price: 62.50, image_url: "https://static.grainger.com/rp/s/is/image/Grainger/3YU78_AS01?$mdmain$", description: "Fiberglass Bow Rake, 3 In.Tines.")
-
-# product.save
-
-# product = Product.new(name: "Milk", price: 2.50, image_url: "https://cdn3.vectorstock.com/i/1000x1000/88/17/realistic-3d-milk-carton-packing-isolated-on-white-vector-21698817.jpg", description: "2% extracted from California's happiest cows, organic and grassfed of course!")
-
-
-# product.save
-
-product = Product.create(name: "Gouda", price: 79.99, image_url: "https://www.igourmet.com/images/products/volendam.jpg", description: "This is our export-quality red wax gouda from Holland.")
+Product.create!([
+  {name: "Burrito Succulent", price: "19.0", image_url: "https://i.etsystatic.com/14339179/r/il/a836b0/2339127554/il_fullxfull.2339127554_soqc.jpg", description: "Long trailing succulents, perfect for hanging!"},
+  {name: "Hanging Succulent Planter", price: "38.0", image_url: "https://sp.apolloboxassets.com/vendor/product/productImages/2019-11-06/KVRNQArray_1(5).jpg", description: "Hanging succulent planter includes 4 pots, plants not included"},
+  {name: "Aloe Vera Plant", price: "43.0", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRCOuk7XUg_TyLsCNc4yO0dLrE_kVjZ878Xb5_8l5xJ0M2YKTJvlVCKZb1QniXcaMD8HpjWHr0&usqp=CAc", description: "Aloe Vera plant, 4 in nursery pot. Requires lots of sun and little water."},
+  {name: "Tiny Cat Planter ", price: "14.0", image_url: "https://i.etsystatic.com/11526084/r/il/72e6f5/927830884/il_fullxfull.927830884_jagz.jpg", description: "2 inch x 2 in x 1.7 inch. Great for Air Plants"},
+  {name: "Tradescantia Zebrina", price: "35.0", image_url: "https://bloomscape.com/wp-content/uploads/2019/04/bloomscape_product-wandering-jew-stone-845x1024.jpg?ver=23771", description: "Comes in a 6 in ceramic pot. Color options are: stone, indigo."},
+  {name: "Golden Pothos 4\"", price: "12.0", image_url: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTGLERrscXj98jgydIFrt7srXqSrD5M713lQk2FkHU8K9nuZk2DCM6cJ3Bri82zk5TW8univRsH2QbfxlgvceuS9BF_PXvFNdErcqfC9pRU&usqp=CAE", description: "Comes in 4 in\" nursey pot."},
+  {name: "Triostart Stormanthe 4 in ", price: "16.0", image_url: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQx2EdGKy_twC4PjvKRztSCnpXDzmhWVFCWLuy1o9U6qzVjdENGE8hlzZQBG-kEJIcwABhkxOCB5YIJyEXwYBixTdJ0M-b81aGY1cm2hnsBIPwNhVU5yHudHA&usqp=CAE", description: "Variegated pink and white prayer plant. Comes in 4 in nursery pot."},
+  {name: "Burgundy Rubber Tree 4 in", price: "14.0", image_url: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSidCJ2oE0KOddxC5v9vflcvy_cncROUovTMWF24b-2O_W_OyzZYr5iXwM11Wfls9nF47BHxZwRFu0WX_YxEGsXfi92pBggooQbJKB-Wcc&usqp=CAE", description: "Comes in 4 in nursery pot"},
+  {name: "Monstera deliciosa, Swiss Cheese Plant", price: "50.0", image_url: "https://www.plantvine.com/plants/Large-Monstera-Delic.jpg", description: "The Monstera Deliciosa or Split Leaf Philodendrum has been a favorite house plant for years – and with good reason. It’s wonderfully easy going, and freshens up any indoor corner. Comes in 10 in black planters pot"},
+  {name: "Dracaena Dorado", price: "198.0", image_url: "https://bloomscape.com/wp-content/uploads/2019/12/bloomscape_product_dracaena-dorado-stone.jpg?ver=108433", description: "The beautiful broadleaf Dracaena Dorado features dark green leaves with light chartreuse borders. Its beauty makes it a delightful indoor plant. But what makes this plant even more appealing are the emerald green canes in varying heights. Truly a ‘living’ conversation piece that will add a unique dimension to your indoor decor! Comes in 12 in ceramic pot."},
+  {name: "Fiddle Leaf Fig", price: "195.0", image_url: "https://bloomscape.com/wp-content/uploads/2019/05/bloomscape_product-fiddle-leaf-fig-stone-2-e1558203089576.jpg?ver=28682", description: "Tall, sculptural, and dramatic. This plant will flourish in the right conditions. The Fiddle Leaf Fig is easily recognizable and loved for its distinctive foliage. This tall, dramatic plant has very large, heavily veined, violin-shaped leaves that grow upright. It’s not bushy, making it a beautiful interior design addition for a brightly lit corner or cozy nook in your home. Comes in 12 in pot."}
+])
