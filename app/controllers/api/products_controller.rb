a9360c9e50b3fcedc557818a580891a0b6d18961
Becca <rrdoukas@gmail.com>
2020-06-08 16:11:34 -0500

    updates app

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,6 +2,21 @@ class Api::ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    if params[:search]
+      @products = @products.where("name iLike ?", "%#{params[:search]}%")
+    end 
+    if params[:discount]
+      @products = @products.where("price < ?", 35)
+    end 
+    if params[:sort] == "price"
+      if params[:sort_order] == "asc"
+        @products = @products.order(:price)
+      elsif params[:sort_order] == "desc"
+        @products = @products.order(price: :desc)
+      end 
+    else 
+      @products = @products.order(:id)
+    end 
     render "index.json.jb"
   end 
 
@@ -18,14 +33,13 @@ class Api::ProductsController < ApplicationController
       image_url: params[:image_url],
       description: params[:description]
     )
-
-  
     if @product.save
       render "show.json.jb"
     else 
       render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
     end 
   end 
+
 
   def update
     @product = Product.find_by(id: params[:id])
@@ -36,15 +50,13 @@ class Api::ProductsController < ApplicationController
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
     
-
-    
     if @product.save
       render "show.json.jb"
     else 
       render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
     end 
-
   end 
+
 
   def destroy
     @product = Product.find_by(id: params[:id])
